8802ede21a76be8af67c12b399a71d69419109e3
Joachim Bakke <github@heiamoss.com>
2018-04-18 12:42:43 +0200

    Fix: polulate all vessels

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const util = require('util');
 const moment = require("moment")
 const utilSK = require('@signalk/nmea0183-utilities');
 const countries = require('i18n-iso-countries')
-var obj = require("./schema.json"); //require empty schema 
+var obj = require("./schema.json"); //require empty schema
 const _ = require('lodash');
 
 
@@ -34,63 +34,52 @@ module.exports = function(app, options) {
     description: "Plugin to respond with arcGIS formatted json containing each vessel's data",
 
     schema: {
-    title: "arcGIS export API",
-    type: "object",
-    properties: {
-    }
-  },
+      title: "arcGIS export API",
+      type: "object",
+      properties: {
+      }
+    },
 
     start: function(options) {},
 
     registerWithRouter: function(router) {
       // http://localhost:3000/ArcGis/API/getJson
-      app.get('/ArcGis/API/getJson', (req, res) => { 
+      app.get('/ArcGis/API/getJson', (req, res) => {
         res.contentType('application/json');
         obj.features = [] //initialize so it does not grow by each call
-         
-        var response = {} 
-        
-        _.values(app.getPath('vessels')).forEach((vessel) => {
-            var attributes = {} //initialize each vessel
-            attributes.MMSI = parseInt(vessel.mmsi)
-            attributes.IMO = parseInt(vessel.imo)
-            attributes.LAT = parseFloat(_.get(vessel, 'navigation.position.value.latitude'))
-            attributes.LON = parseFloat(_.get(vessel, 'navigation.position.value.longitude'))
-            attributes.SPEED = parseFloat(utilSK.transform(_.get(vessel, 'navigation.speedOverGround.value'), 'ms', 'knots'))
-            attributes.HEADING = parseFloat(utilSK.transform(_.get(vessel, 'navigation.headingTrue.value'), 'rad', 'deg'))
-            attributes.COURSE = parseFloat(utilSK.transform(_.get(vessel, 'navigation.courseOverGroundTrue.value'), 'rad', 'deg'))
-            attributes.STATUS = _.get(vessel, 'navigation.state.value')
-            attributes.TIMESTAMP_ = moment(_.get(vessel, 'navigation.position.timestamp')).unix()
-            attributes.SOURCE = _.get(vessel, 'navigation.position.$source')
-            attributes.SHIPNAME = vessel.name
-            attributes.SHIPTYPE = _.get(vessel, 'design.aisShipType.value.name')
-            attributes.CALLSIGN = _.get(vessel, 'communication.callsignVhf')
-            attributes.FLAG = countries.getName(vessel.flag, "en")
-            
-            var geometry = {}
-            geometry.x = attributes.LAT
-            geometry.y = attributes.LON
-            
-            response.attributes = attributes
-            response.geometry = geometry
+
+        var response = {}
+        _.values(app.getPath('vessels')).map((vessel) => {
+          var attributes = {} //initialize each vessel
+          attributes.MMSI = parseInt(vessel.mmsi)
+          attributes.IMO = parseInt(vessel.imo)
+          attributes.LAT = parseFloat(_.get(vessel, 'navigation.position.value.latitude'))
+          attributes.LON = parseFloat(_.get(vessel, 'navigation.position.value.longitude'))
+          attributes.SPEED = parseFloat(utilSK.transform(_.get(vessel, 'navigation.speedOverGround.value'), 'ms', 'knots'))
+          attributes.HEADING = parseFloat(utilSK.transform(_.get(vessel, 'navigation.headingTrue.value'), 'rad', 'deg'))
+          attributes.COURSE = parseFloat(utilSK.transform(_.get(vessel, 'navigation.courseOverGroundTrue.value'), 'rad', 'deg'))
+          attributes.STATUS = _.get(vessel, 'navigation.state.value')
+          attributes.TIMESTAMP_ = moment(_.get(vessel, 'navigation.position.timestamp')).unix()
+          attributes.SOURCE = _.get(vessel, 'navigation.position.$source')
+          attributes.SHIPNAME = vessel.name
+          attributes.SHIPTYPE = _.get(vessel, 'design.aisShipType.value.name')
+          attributes.CALLSIGN = _.get(vessel, 'communication.callsignVhf')
+          attributes.FLAG = countries.getName(vessel.flag, "en")
+
+          var geometry = {}
+          geometry.x = attributes.LAT
+          geometry.y = attributes.LON
+
+          response.attributes = attributes
+          response.geometry = geometry
+          obj.features.push({ attributes, geometry })
         })
-
-
-        obj.features.push(response)
-
         res.send(JSON.stringify(obj, null, 4))
       })
-
-
     },
 
-
     stop: function() {
-
       debug("Stopped")
     }
   }
-
-
 }
-
